403585f7b6e54b4c34369e1cb35fb72890568286
Jesse Callahan <jessetylercallahan@gmail.com>
2020-12-30 13:49:03 -0800

    Updates

---
+++ precache-manifest.f64097873976f9361983b031220d4fa0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ee8f5cfbf8e89bd76b4d4e576483cfc",
+    "url": "/Marcins-Movie-Proj/index.html"
+  },
+  {
+    "revision": "babab0047ad52cf3a3d6",
+    "url": "/Marcins-Movie-Proj/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a13363f7fd703e091ac8",
+    "url": "/Marcins-Movie-Proj/static/css/main.02dacecc.chunk.css"
+  },
+  {
+    "revision": "babab0047ad52cf3a3d6",
+    "url": "/Marcins-Movie-Proj/static/js/2.f9c66a23.chunk.js"
+  },
+  {
+    "revision": "a13363f7fd703e091ac8",
+    "url": "/Marcins-Movie-Proj/static/js/main.8a5e6061.chunk.js"
+  },
+  {
+    "revision": "731e28ae9556f3958540",
+    "url": "/Marcins-Movie-Proj/static/js/runtime-main.ba987069.js"
+  },
+  {
+    "revision": "ea872ff0d46d13eea232953664edc01d",
+    "url": "/Marcins-Movie-Proj/static/media/IndubitablyNF.ea872ff0.ttf"
+  },
+  {
+    "revision": "985589899c4bf9282be466bab05a0a94",
+    "url": "/Marcins-Movie-Proj/static/media/projector-1.98558989.jpg"
+  }
+]);
